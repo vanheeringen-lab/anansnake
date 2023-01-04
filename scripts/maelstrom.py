f2bf51de6affc32af27b02e0f6829b4adfc02e11
JGASmits <jossmits_3@hotmail.com>
2023-01-04 16:44:21 +0100

    TopVarPeaksMaelstrom

--- scripts/maelstrom.py
+++ scripts/maelstrom.py
@@ -38,6 +38,12 @@ with open(str(log), "a") as f:
         df = np.log2(df + 1)
         df = qnorm.quantile_normalize(df)
 
+        #reduce df lenght to only highly variable rows if df length > 50.000
+        if len(df) > 50000:
+            df['variance'] = df.var(axis = 1)
+            df = df.sort_values('variance', ascending = False)[1:50000]
+            df = df.drop('variance', axis = 1)
+
         # save count matrix to temporary file
         os.makedirs(outdir, exist_ok=True)
         tmp = os.path.join(outdir, "maelstrom_input.tsv")
